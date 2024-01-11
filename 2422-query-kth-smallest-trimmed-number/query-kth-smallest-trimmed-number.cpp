a89f9fdad408762ceacf3391a67083bc02115ffe
Hardik <119474920+hardikk65@users.noreply.github.com>
2024-01-12 04:33:35 +0530

    Time: 1148 ms (62.70%) | Memory: 421.4 MB (37.75%) - LeetSync

---
+++ 2422-query-kth-smallest-trimmed-number/query-kth-smallest-trimmed-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> smallestTrimmedNumbers(vector<string>& nums, vector<vector<int>>& queries) {
+        vector<int> ans;
+        int size = nums[0].size();
+        for(int i=0;i<queries.size();i++){
+            int trim = queries[i][1];
+            int k = queries[i][0];
+            vector<pair<string,int>> s;
+            for(int j=0;j<nums.size();j++){
+                string sub = nums[j].substr(size-trim);
+                s.push_back(make_pair(sub,j));
+            }
+            
+            sort(s.begin(),s.end());
+            
+            ans.push_back(s[k-1].second);
+            
+        }
+        return ans;
+    }
+};
