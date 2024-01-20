a4e1ebd6354dabb24f0331c66b48e37b58c9cab3
Hardik <119474920+hardikk65@users.noreply.github.com>
2024-01-20 23:02:54 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (29.01%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -1,27 +1,15 @@
 class Solution {
 public:
     int findTheWinner(int n, int k) {
+
+        int ans = 0;
+        for(int i=1; i<=n; i++){
+            ans = (ans + k) % i;
+        }
+        return ans + 1;
+
         
-        vector<int> friends;
-        for(int i = 1;i <= n;i++){
-            friends.push_back(i);
-        }
-
-        int start = 0;
-        while(friends.size() != 1){
-
-            int final_remove = (start + k - 1)%friends.size();
-
-            start = (start + k)%friends.size();
-            friends.erase(friends.begin() + final_remove);
-            if(start != 0){
-                start = start - 1;
-            }
-
-        }
-
-
-        return friends[0];
+        
         
     }
 };
